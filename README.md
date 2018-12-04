6679b02353174bb8406a318487aa2f421086f24e
Goessi <qinj1994@163.com>
2018-12-04 22:32:04 +0100

    first version
    __init__, __add__, __str__

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Master_Thesis
+
+|  Author  |         Email          |
+|----------|------------------------|
+| Jing QIN | Jing.QIN94@outlook.com |
+
+## introduction
+### first version of master thesis codes
+### add __init__, __add__, __str__ [date: 04/12/2018]
---
+++ quaternion_first_version.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 22:04:11 2018
+
+first version of Master thesis codes, initial model
+
+@author: JingQIN
+"""
+
+class Quaternion(object):
+    def __init__(self, a, b, c, d):
+        assert type(a) == float and type(b) == float and type(c) == float and type(d) == float
+        
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+    
+    def __add__(self, other):
+        return Quaternion(self.a+other.a, self.b+other.b, self.c+other.c, self.d+other.d)
+    
+    def __str__(self):
+        return str(self.a) + ' + ' + str(self.b) + 'i + ' + str(self.c) + 'j + ' + str(self.d) + 'k'
+    
+A = Quaternion(1.0,2.0,3.0,4.0)
+B = Quaternion(5.0,6.0,7.0,8.0)
+C = A + B
+print(A)
+print(B)
+print(C)
