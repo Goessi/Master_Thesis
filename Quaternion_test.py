dea6198911d0d3d5f59dd540238ffe2bc39156bf
Jing QIN <qinj1994@163.com>
2018-12-28 20:13:20 +0100

    cmparision between DCM and Quaternion

--- Quaternion_test.py
+++ Quaternion_test.py
@@ -53,49 +53,49 @@ print(E.norm())
 print("-------test on conjugate-------")
 E = Quaternion(1.0, 2.0, 3.0, 4.0)
 print(E.conj())
-print("-------test on rotation pricision------")
-X1 = []
-X2 = []
-MEANDIFF = []
-MINDIFF = []
-MAXDIFF = []
-for N in range (1,30):
-    meandiff = 0.0
-    maxdiff = 0.0
-    mindiff = 1e9
-    for r in range(0,100):
-        y = random.random()
-        p = Quaternion(0.0, 0.0, y, 0.0)
-        p_zero = Quaternion(0.0, 0.0, y, 0.0)
-        steps= 180 * N
-        for i in range(0, steps):
-            p = p.rotator(np.pi / steps,[1.0, 0.0, 0.0])
-        for i in range(0, steps):
-            p = p.rotator(np.pi / steps,[0.0, 1.0, 0.0])
-        for i in range(0, steps):
-            p = p.rotator(np.pi / steps,[0.0, 0.0, 1.0])               
-        x = (p_zero - p).norm()
-        meandiff += x
-        mindiff = min(mindiff, x)
-        maxdiff = max(maxdiff, x)
-    X1.append(1.0 / N)
-    X2.append(N)
-    MEANDIFF.append(meandiff / 100) # remember to change according to different times
-    MINDIFF.append(mindiff)
-    MAXDIFF.append(maxdiff)
-
-fig, ax = plt.subplots()
-ax.semilogy(X1, MEANDIFF, 'k--', label='Mean', color = 'red')
-ax.semilogy(X1, MINDIFF, 'k:', label='MIN', color = 'green')
-ax.semilogy(X1, MAXDIFF, 'k', label='MAX', color = 'blue')
-ax.set_xlabel("Times")
-ax.set_ylabel("Differences")
-ax.set_title("Differences in 1000 steps, 30 rounds")
-legend = ax.legend(loc=2, bbox_to_anchor=(1.05,1.0),borderaxespad = 0.)
-legend.get_frame().set_facecolor('C0')
-plt.show()
-fig.set_size_inches(18.5, 10.5)
-fig.savefig('test_Quaternion_100.png', dpi=100)
+#print("-------test on rotation precision------")
+#X1 = []
+#X2 = []
+#MEANDIFF = []
+#MINDIFF = []
+#MAXDIFF = []
+#for N in range (1,30):
+#    meandiff = 0.0
+#    maxdiff = 0.0
+#    mindiff = 1e9
+#    for r in range(0,100):
+#        y = random.random()
+#        p = Quaternion(0.0, 0.0, y, 0.0)
+#        p_zero = Quaternion(0.0, 0.0, y, 0.0)
+#        steps= 180 * N
+#        for i in range(0, steps):
+#            p = p.rotator(np.pi / steps,[1.0, 0.0, 0.0])
+#        for i in range(0, steps):
+#            p = p.rotator(np.pi / steps,[0.0, 1.0, 0.0])
+#        for i in range(0, steps):
+#            p = p.rotator(np.pi / steps,[0.0, 0.0, 1.0])               
+#        x = (p_zero - p).norm()
+#        meandiff += x
+#        mindiff = min(mindiff, x)
+#        maxdiff = max(maxdiff, x)
+#    X1.append(1.0 / N)
+#    X2.append(N)
+#    MEANDIFF.append(meandiff / 100) # remember to change according to different times
+#    MINDIFF.append(mindiff)
+#    MAXDIFF.append(maxdiff)
+#
+#fig, ax = plt.subplots()
+#ax.semilogy(X1, MEANDIFF, 'k--', label='Mean', color = 'red')
+#ax.semilogy(X1, MINDIFF, 'k:', label='MIN', color = 'green')
+#ax.semilogy(X1, MAXDIFF, 'k', label='MAX', color = 'blue')
+#ax.set_xlabel("Times")
+#ax.set_ylabel("Differences")
+#ax.set_title("Differences in 100 steps, 30 rounds")
+#legend = ax.legend(loc=2, bbox_to_anchor=(1.05,1.0),borderaxespad = 0.)
+#legend.get_frame().set_facecolor('C0')
+#plt.show()
+#fig.set_size_inches(18.5, 10.5)
+#fig.savefig('test_Quaternion_100.png', dpi=100)
 print("-------test on toDCM-------")
 E = Quaternion(0.0, 1.0, 1.0, 1.0)
 D = E.toDCM()
@@ -135,7 +135,7 @@ def DCMtoQuaternion(aListOfLists):
         qy = (aListOfLists[2][1] + aListOfLists[1][2]) / (4 * q0z)
         return Quaternion(qs, qx, qy, q0z)
 print(DCMtoQuaternion(D))
-print("-------test on DCM rotation-------")
+#print("-------test on DCM rotation-------")
 def computeDCM(theta, vectors):
     '''
     compute standard DCM, a list of lists, 3*3 matrix
@@ -160,41 +160,41 @@ def computeDCM(theta, vectors):
     DCM[2][1] = f2 * f3 * (1 - cosTheta) - f1 * sinTheta
     DCM[2][2] = cosTheta + pow(f3, 2) * (1 - cosTheta)
     return DCM
-
-X1 = []
-X2 = []
-MEANDIFF = []
-MINDIFF = []
-MAXDIFF = []
-for N in range (1,30):
-    meandiff = 0.0
-    maxdiff = 0.0
-    mindiff = 1e9
-    for r in range(0, 1000):
-        x = random.random()
-        y = random.random()
-        z = random.random()
-        v = [x, y, z]
-        v_zero = [x, y, z]
-        steps= 180 * N
-        m1 = computeDCM(np.pi/ steps, [1, 0, 0])
-        m2 = computeDCM(np.pi/ steps, [0, 1, 0])
-        m3 = computeDCM(np.pi/ steps, [0, 0, 1])
-        for i in range(0, steps):
-            v = np.dot(m1, v)
-        for j in range(0, steps):
-            v = np.dot(m2, v)
-        for k in range(0, steps):
-            v = np.dot(m3, v)              
-        x = math.sqrt(pow(v[0] - v_zero[0], 2) + pow(v[1] - v_zero[1], 2) + pow(v[2] - v_zero[2], 2))
-        meandiff += x
-        mindiff = min(mindiff, x)
-        maxdiff = max(maxdiff, x)
-    X1.append(1.0 / N)
-    X2.append(N)
-    MEANDIFF.append(meandiff / 1000) # remember to change according to different times
-    MINDIFF.append(mindiff)
-    MAXDIFF.append(maxdiff)
+#
+#X1 = []
+#X2 = []
+#MEANDIFF = []
+#MINDIFF = []
+#MAXDIFF = []
+#for N in range (1,30):
+#    meandiff = 0.0
+#    maxdiff = 0.0
+#    mindiff = 1e9
+#    for r in range(0, 1000):
+#        x = random.random()
+#        y = random.random()
+#        z = random.random()
+#        v = [x, y, z]
+#        v_zero = [x, y, z]
+#        steps= 180 * N
+#        m1 = computeDCM(np.pi/ steps, [1, 0, 0])
+#        m2 = computeDCM(np.pi/ steps, [0, 1, 0])
+#        m3 = computeDCM(np.pi/ steps, [0, 0, 1])
+#        for i in range(0, steps):
+#            v = np.dot(m1, v)
+#        for j in range(0, steps):
+#            v = np.dot(m2, v)
+#        for k in range(0, steps):
+#            v = np.dot(m3, v)              
+#        x = math.sqrt(pow(v[0] - v_zero[0], 2) + pow(v[1] - v_zero[1], 2) + pow(v[2] - v_zero[2], 2))
+#        meandiff += x
+#        mindiff = min(mindiff, x)
+#        maxdiff = max(maxdiff, x)
+#    X1.append(1.0 / N)
+#    X2.append(N)
+#    MEANDIFF.append(meandiff / 1000) # remember to change according to different times
+#    MINDIFF.append(mindiff)
+#    MAXDIFF.append(maxdiff)
 
 fig, ax = plt.subplots()
 ax.semilogy(X1, MEANDIFF, 'k--', label='Mean', color = 'red')
@@ -203,13 +203,79 @@ ax.semilogy(X1, MAXDIFF, 'k', label='MAX', color = 'blue')
 ax.set_xlabel("Times")
 ax.set_ylabel("Differences")
 ax.set_title("Differences in 1000 steps, 30 rounds")
-legend = ax.legend(loc=2, bbox_to_anchor=(1.05,1.0),borderaxespad = 0.)
+legend = ax.legend(loc = 2, bbox_to_anchor = (1.05, 1.0), borderaxespad = 0.)
 legend.get_frame().set_facecolor('C0')
 plt.show()
 fig.set_size_inches(18.5, 10.5)
-fig.savefig('test_DCM_1000.png', dpi=100)
+fig.savefig('test_DCM_1000.png', dpi = 100)
 
+print("-------compare output differences in Quaternion and DCM-------")
+# initialize some list to store data
+X1 = []
+X2 = []
+MEANDIFF_Q = []
+MINDIFF_Q = []
+MAXDIFF_Q = []
+MEANDIFF_DCM = []
+MINDIFF_DCM = []
+MAXDIFF_DCM = []
 
+for N in range (1,30):
+    meandiff_Q = 0.0
+    maxdiff_Q = 0.0
+    mindiff_Q = 1e9
+    meandiff_DCM = 0.0
+    maxdiff_DCM = 0.0
+    mindiff_DCM = 1e9
+    for r in range(0,1000):
+        x = random.random()
+        y = random.random()
+        z = random.random()
+        p = Quaternion(0.0, x, y, z)
+        p_zero = Quaternion(0.0, x, y, z)
+        v = [x, y, z]
+        v_zero = [x, y, z]
+        steps= 180 * N
+        m1 = computeDCM(np.pi/ steps, [1, 0, 0])
+        m2 = computeDCM(np.pi/ steps, [0, 1, 0])
+        m3 = computeDCM(np.pi/ steps, [0, 0, 1])       
+        for i in range(0, steps):
+            p = p.rotator(np.pi / steps,[1.0, 0.0, 0.0])
+            v = np.dot(m1, v)
+        for i in range(0, steps):
+            p = p.rotator(np.pi / steps,[0.0, 1.0, 0.0])
+            v = np.dot(m2, v)
+        for i in range(0, steps):
+            p = p.rotator(np.pi / steps,[0.0, 0.0, 1.0])
+            v = np.dot(m3, v)               
+        x_Q = (p_zero - p).norm()
+        meandiff_Q += x_Q
+        mindiff_Q = min(mindiff_Q, x_Q)
+        maxdiff_Q = max(maxdiff_Q, x_Q)
+        x_DCM = math.sqrt(pow(v[0] - v_zero[0], 2) + pow(v[1] - v_zero[1], 2) + pow(v[2] - v_zero[2], 2))
+        meandiff_DCM += x_DCM
+        mindiff_DCM = min(mindiff_DCM, x_DCM)
+        maxdiff_DCM = max(maxdiff_DCM, x_DCM)
+    X1.append(1.0 / N)
+    X2.append(N)
+    MEANDIFF_Q.append(meandiff_Q / 1000)
+    MINDIFF_Q.append(mindiff_Q)
+    MAXDIFF_Q.append(maxdiff_Q)
+    MEANDIFF_DCM.append(meandiff_DCM / 1000)
+    MINDIFF_DCM.append(mindiff_DCM)
+    MAXDIFF_DCM.append(maxdiff_DCM)
+    
+fig, ax = plt.subplots()
+ax.semilogy(X1, MEANDIFF_Q, 'k--', label='Mean_Quaternion', color = 'red')
+ax.semilogy(X1, MEANDIFF_DCM, 'k--', label='Mean_DCM', color = 'green')
+ax.set_xlabel("Times")
+ax.set_ylabel("Differences")
+ax.set_title("Differences between DCM and Quaternion in 1000 steps, 30 rounds")
+legend = ax.legend(loc = 2, bbox_to_anchor = (1.05, 1.0), borderaxespad = 0.)
+legend.get_frame().set_facecolor('C0')
+plt.show()
+fig.set_size_inches(18.5, 10.5)
+fig.savefig('test_DCM_Quaternion_1000.png', dpi = 100)
 
 
 
