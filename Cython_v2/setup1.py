0e023dfb2fd4601f680e11b1ea2fe2d0941db1a2
Jing QIN <qinj1994@163.com>
2019-03-02 21:16:20 +0100

    v2

--- Cython_v2/setup1.py
+++ Cython_v2/setup1.py
@@ -1,6 +1,8 @@
-from distutils.core import setup
-from Cython.Build import cythonize 
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("Quaternion.pyx")
+    ext_modules = cythonize("Quaternion.pyx"),
+    include_dirs=[numpy.get_include()]
 )
--- Cython_v2/setup2.py
+++ Cython_v2/setup2.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize 
-
+import numpy
 setup(
-    ext_modules = cythonize("Quaternion_test.pyx")
+    ext_modules = cythonize("Quaternion_test.pyx"),
+    include_dirs=[numpy.get_include()]
 )
