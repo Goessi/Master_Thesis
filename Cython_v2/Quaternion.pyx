0f73cf78c310cf9102c99c539fab4225c2619a0f
Jing QIN <qinj1994@163.com>
2019-03-04 09:32:50 +0100

    v2

--- Cython_v2/Quaternion.pyx
+++ Cython_v2/Quaternion.pyx
@@ -7,18 +7,11 @@ first version of Master thesis codes, initial model
 @author: JingQIN
 """
 
-cdef extern from "math.h":
-    double sqrt(double m)
-    float cosf(float theta)
-    float sinf(float theta)
-from numpy cimport ndarray
-cimport numpy as np
-cimport cython
-from libcpp.string cimport string
-from libcpp.vector cimport vector
+import math
+import numpy as np
+import decimal
 
-
-cdef class Quaternion:
+class Quaternion(object):
     """class of Quaternion that do the simple operations
     
     Attributes:
@@ -27,76 +20,57 @@ cdef class Quaternion:
         c -- a float parameter of fundamental quaternion unit j
         d -- a float parameter of fundamental quaternion unit k
     """    
-    cdef float a
-    cdef float b
-    cdef float c
-    cdef float d
-    cdef dict __dict__
-    
-    def __init__(self, float aa, float bb, float cc, float dd):
+    def __init__(self, a, b, c, d):
         '''initial Quaternion class with 4 floats'''
         #assert type(a) == decimal.Decimal and type(b) == decimal.Decimal and type(c) == decimal.Decimal and type(d) == decimal.Decimal
         
-        self.a = aa
-        self.b = bb
-        self.c = cc
-        self.d = dd        
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d        
     
-    def __add__(self, Quaternion other):
+    def __add__(self, other):
         '''compute Quaternion objects addition
         
         arguments:
             other -- another Quaternion object
         '''
-        if (isinstance(self, Quaternion) and isinstance(other, Quaternion)):
-            return Quaternion(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
-        else:
-            return NotImplemented
+        return Quaternion(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
     
-    def __sub__(self, Quaternion other):
+    def __sub__(self, other):
         '''compute Quaternion objects subtraction
         
         arguments:
             other -- another Quaternion object
         '''
-        if (isinstance(self, Quaternion) and isinstance(other, Quaternion)):
-            return Quaternion(self.a - other.a, self.b - other.b, self.c - other.c, self.d - other.d)
-        else:
-            return NotImplemented        
+        return Quaternion(self.a - other.a, self.b - other.b, self.c - other.c, self.d - other.d)
         
-    def __mul__(self, Quaternion other):
+    def __mul__(self, other):
         '''compute Quaternion objects multiple
         
         arguments:
             other -- another Quaternion object
         '''
-        
-        if(isinstance(self, Quaternion) and isinstance(other, Quaternion)):
-            a = self.a * other.a - self.b * other.b - self.c * other.c - self.d * other.d
-            b = self.a * other.b + self.b * other.a + self.c * other.d - self.d * other.c
-            c = self.a * other.c - self.b * other.d + self.c * other.a + self.d * other.b
-            d = self.a * other.d + self.b * other.c - self.c * other.b + self.d * other.a
-            return Quaternion(a, b, c, d)
-        else:
-            return NotImplemented 
-        
+        a = self.a * other.a - self.b * other.b - self.c * other.c - self.d * other.d
+        b = self.a * other.b + self.b * other.a + self.c * other.d - self.d * other.c
+        c = self.a * other.c - self.b * other.d + self.c * other.a + self.d * other.b
+        d = self.a * other.d + self.b * other.c - self.c * other.b + self.d * other.a
+        return Quaternion(a, b, c, d)
     
-    def scalar_mul(self, float scalar):
+    def scalar_mul(self, scalar):
         '''compute Quaternion objects multiple
         
         arguments:
             scalar -- a scalar
         '''
-        if(isinstance(self, Quaternion)):
-            a = self.a * scalar
-            b = self.b * scalar
-            c = self.c * scalar
-            d = self.d * scalar
-            return Quaternion(a, b, c, d)
-        else:
-            return NotImplemented
+        a = self.a * scalar
+        b = self.b * scalar
+        c = self.c * scalar
+        d = self.d * scalar
+        
+        return Quaternion(a, b, c, d)
     
-    def dot(self, Quaternion other):
+    def dot(self, other):
         '''compute Quaternion objects dot production
         
         arguments:
@@ -108,14 +82,14 @@ cdef class Quaternion:
         '''
         compute Quaternion object norm
         '''
-        return sqrt(pow(self.a, 2) + pow(self.b, 2) + pow(self.c, 2) + pow(self.d, 2))
+        return math.sqrt(pow(self.a, 2) + pow(self.b, 2) + pow(self.c, 2) + pow(self.d, 2))
     
     def norm_q(self):
         '''
         compute normalized Quaternion
         '''
-        cdef float mynorm = self.norm()
-        cdef Quaternion my_norm_q = Quaternion(self.a / mynorm, self.b / mynorm, self.c / mynorm, self.d / mynorm)
+        mynorm = self.norm()
+        my_norm_q = Quaternion(self.a / mynorm, self.b / mynorm, self.c / mynorm, self.d / mynorm)
         return my_norm_q
     
     def conj(self):
@@ -128,7 +102,7 @@ cdef class Quaternion:
         d = -self.d
         return Quaternion(a, b, c, d)
     
-    def rotator(self, float theta, vector[float] vectors):
+    def rotator(self, theta, vectors):
         '''
         from angle and vectors, compute a quaternion
         
@@ -137,14 +111,14 @@ cdef class Quaternion:
             vectors -- indicates rotation aixs, list, like [1, 0, 0]
         '''
         
-        cdef float sum_v = sum([v * v for v in vectors])
-        cdef float norm_v = sqrt(sum_v)
+        sum_v = sum([v * v for v in vectors])
+        norm_v = math.sqrt(sum_v)
         vectors = [v / norm_v for v in vectors]
             
-        cdef float a = cosf(theta / 2.)
-        cdef float b = vectors[0] * sinf(theta / 2.)
-        cdef float c = vectors[1] * sinf(theta / 2.)
-        cdef float d = vectors[2] * sinf(theta / 2.)
+        a = math.cos(theta / 2.)
+        b = vectors[0] * math.sin(theta / 2.)
+        c = vectors[1] * math.sin(theta / 2.)
+        d = vectors[2] * math.sin(theta / 2.)
         
         r = Quaternion(a, b, c, d)
         
@@ -155,31 +129,31 @@ cdef class Quaternion:
         compute a Quaternion object to a DCM, a list of lists
         specifically, a list of three 1*3 list, normalized 
         '''
-        cdef float q0 = self.a
-        cdef float q1 = self.b
-        cdef float q2 = self.c
-        cdef float q3 = self.d
+        q0 = self.a
+        q1 = self.b
+        q2 = self.c
+        q3 = self.d
         
-        cdef float C11 = pow(q0, 2) + pow(q1, 2) - pow(q2, 2) - pow(q3, 2)
-        cdef float C12 = 2 * (q1 * q2 + q0 * q3)
-        cdef float C13 = 2 * (q1 * q3 - q0 * q2)        
-        cdef float C21 = 2 * (q1 * q2 - q0 * q3)
-        cdef float C22 = pow(q0, 2) - pow(q1, 2) + pow(q2, 2) - pow(q3, 2)
-        cdef float C23 = 2 * (q2 * q3 + q0 * q1)        
-        cdef float C31 = 2 * (q1 * q3 + q0 * q2)
-        cdef float C32 = 2 * (q2 * q3 - q0 * q1)
-        cdef float C33 = pow(q0, 2) - pow(q1, 2) - pow(q2, 2) + pow(q3, 2)
-        cdef float C3_norm = sqrt(pow(C31, 2) + pow(C32, 2) + pow(C33, 2))
-        cdef float C1_norm = sqrt(pow(C11, 2) + pow(C12, 2) + pow(C13, 2))
-        cdef float C2_norm = sqrt(pow(C21, 2) + pow(C22, 2) + pow(C23, 2))
+        C11 = pow(q0, 2) + pow(q1, 2) - pow(q2, 2) - pow(q3, 2)
+        C12 = 2 * (q1 * q2 + q0 * q3)
+        C13 = 2 * (q1 * q3 - q0 * q2)        
+        C21 = 2 * (q1 * q2 - q0 * q3)
+        C22 = pow(q0, 2) - pow(q1, 2) + pow(q2, 2) - pow(q3, 2)
+        C23 = 2 * (q2 * q3 + q0 * q1)        
+        C31 = 2 * (q1 * q3 + q0 * q2)
+        C32 = 2 * (q2 * q3 - q0 * q1)
+        C33 = pow(q0, 2) - pow(q1, 2) - pow(q2, 2) + pow(q3, 2)
+        C3_norm = math.sqrt(pow(C31, 2) + pow(C32, 2) + pow(C33, 2))
+        C1_norm = math.sqrt(pow(C11, 2) + pow(C12, 2) + pow(C13, 2))
+        C2_norm = math.sqrt(pow(C21, 2) + pow(C22, 2) + pow(C23, 2))
         DCM = [[C11 / C1_norm, C12 / C1_norm, C13 / C1_norm],[C21 / C2_norm, C22 / C2_norm, C23 / C2_norm],[C31 / C3_norm, C32 / C3_norm, C33 / C3_norm]]
-        return ndarray(DCM)
+        return np.array(DCM)
         
         
     def __str__(self):
         ''' document printing'''
         parameters = {'':self.a, 'i':self.b, 'j':self.c, 'k':self.d}
-        cdef int count = 0
+        count = 0
         w = ''
         for k,v in parameters.items():
             if v != 0:
